1303eff6b79bfbeb7b2703ef33439a86d164bc59
Alejandro Guevara <A00834438@tec.mx>
2026-01-23 20:26:59 -0600

    Game integrated into web app

--- WebAppTecTreasure/Program.cs
+++ WebAppTecTreasure/Program.cs
@@ -1,4 +1,6 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.StaticFiles;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -22,14 +24,23 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+
+var provider = new FileExtensionContentTypeProvider();
+provider.Mappings.Remove(".data");
+provider.Mappings[".data"] = "application/octet-stream";
+provider.Mappings.Remove(".wasm");
+provider.Mappings[".wasm"] = "application/wasm";
+provider.Mappings.Remove(".symbols.json");
+provider.Mappings[".symbols.json"] = "application/octet-stream";
+app.UseStaticFiles(new StaticFileOptions { ContentTypeProvider = provider });
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseSession();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
 app.MapRazorPages();
 
